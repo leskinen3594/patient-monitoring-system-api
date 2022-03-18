68f189d1e1c6e6b775dd4b2bb90563c67287f06c
leskinen3594 <rikuto1412@gmail.com>
2022-03-18 23:22:55 +0700

    first deploy to heroku

--- api/main.py
+++ api/main.py
@@ -16,8 +16,8 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host = "0.0.0.0", port = 8000, reload = True)
+# if __name__ == '__main__':
+#     uvicorn.run("main:app", host = "0.0.0.0", port = 8000, reload = True)
 
 # Run with cli
 # uvicorn main:app --reload --host 0.0.0.0 --port 5000
